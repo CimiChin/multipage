a25ab727f79af43de272b79f3c8245de4d493dff
CimiChin <151364311+CimiChin@users.noreply.github.com>
2025-05-22 09:25:35 +0700

    Create App.py

---
+++ App.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Aplikasi Multi Halaman", layout="wide")
+
+st.title("🏠 Halaman Utama")
+st.write("Selamat datang di aplikasi Streamlit multi-halaman!")
+st.write("Gunakan sidebar untuk berpindah ke halaman lain.")
